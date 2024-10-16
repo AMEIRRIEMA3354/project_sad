592fcf8477681c91d719b6c80c85e52b6ba290e5
AMEIRRIEMA3354 <117885531+AMEIRRIEMA3354@users.noreply.github.com>
2024-10-16 20:08:10 +0800

    Create config.php

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "aduanpengguna";
+$connect = mysqli_connect($hostname,$username,$password,$database) or die('failed to connect');
+?>
